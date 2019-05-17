4c6565eb2f83c10036ccfc1998bb108b7dab0dcb
wellingtonsilva04 <wel.dss@gmail.com>
2019-05-17 19:24:02 -0300

    adicionado as soluções do exercício e o app CalcularSoma

--- Exercícios/calculaSoma/paginas/home.js
+++ Exercícios/calculaSoma/paginas/home.js
@@ -5,6 +5,8 @@ import {
     StyleSheet,
     TextInput,
     TouchableOpacity,
+    ScrollView,
+    KeyboardAvoidingView
 } from 'react-native';
 import { Constants } from 'expo';
 
@@ -22,44 +24,61 @@ export default class App extends React.Component {
             resultado: ''
         }
     }
-
+    limpar = () => {
+        this.setState({ resultado: "" })
+    }
     somar = () => {
         let { numero1, numero2 } = this.state;
-        
+
         if (numero1 !== '' && numero2 !== '') {
             let resultado = parseFloat(numero1) + parseFloat(numero2)
             this.setState({ resultado });
-            this.setState({numero1: '', numero2: ''});
-            
+            this.setState({ numero1: '', numero2: '' });
+
         }
-        
+
 
     }
 
     render() {
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior="padding">
                 <View style={styles.containerResultado}>
                     <Text style={styles.textResultado}>{this.state.resultado}</Text>
                 </View>
-                <TextInput placeholder='Digite um nome'
+                <TextInput placeholder='Digite um número'
                     onChangeText={(texto) => this.setState({ numero1: texto })}
+                    autoFocus
+                    onSubmitEditing={() => { this._text2.focus() }}
                     keyboardType='numeric'
-                    value = {this.state.numero1}
+                    value={this.state.numero1}
                     style={styles.inputNumero}
                 />
                 <TextInput placeholder='Digite outro número'
                     onChangeText={(texto) => this.setState({ numero2: texto })}
                     keyboardType='numeric'
-                    value = {this.state.numero2}
+                    ref={(text2) => this._text2 = text2}
+                    onSubmitEditing={() => { this.somar() }}
+                    value={this.state.numero2}
                     style={styles.inputNumero}
                 />
-                <TouchableOpacity
-                    style={styles.buttomCalcular}
-                    onPress={() => this.somar()}>
-                    <Text style={styles.textButtomCalcular}>Calcular</Text>
-                </TouchableOpacity>
-            </View>
+                <View style={styles.containerButtoms}>
+                    <TouchableOpacity
+                        style={styles.buttomLimpar}
+                        onPress={() => this.limpar()}>
+                        <Text style={styles.textButtomlimpar}>C</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.buttomCalcular}
+                        onPress={() => this.somar()}>
+                        <Text style={styles.textButtomCalcular}>Calcular</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </KeyboardAvoidingView>
+
         );
     };
 }
@@ -67,11 +86,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         paddingTop: Constants.statusBarHeight,
         backgroundColor: '#e0ebeb',
         padding: 8,
+        justifyContent: "center"
     },
     containerResultado: {
         backgroundColor: '#ffffff',
@@ -80,8 +99,15 @@ const styles = StyleSheet.create({
         height: 50,
         alignItems: 'flex-end',
         paddingHorizontal: 20,
+        
         marginBottom: 10
 
+    },
+    containerButtoms: {
+        alignSelf: "stretch",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly",
     },
     inputNumero: {
         marginTop: 10,
@@ -98,6 +124,14 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 5,
         marginVertical: 10
+    },
+    buttomLimpar: {
+        backgroundColor: '#adc2eb',
+        borderWidth: 1,
+        padding: 5,
+        width: 35,
+        marginVertical: 10,
+        alignItems: "center"
     },
 
 
